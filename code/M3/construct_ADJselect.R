443f41710451304c738ecf2bf434af84e4d68ae2
qiqi <108138869+loofla@users.noreply.github.com>
2025-08-26 20:19:22 +0800

    Update construct_ADJselect.R
    add structured comments

--- code/M3/construct_ADJselect.R
+++ code/M3/construct_ADJselect.R
@@ -1,4 +1,26 @@
-
+##############################################################################
+# Levels and trends estimate of sex ratio at birth for seven provinces of Pakistan from 
+# 1980 to 2020 with scenario-based probabilistic projections 
+# of missing female birth to 2050: A Bayesian modeling approach
+# Code constructed by: Fengqing CHAO
+# Code last revised by: Qiqi Qiang on 26 Aug 2025
+# construct_ADJselect.R
+# 
+# This script extracts posterior samples of the adjustment parameters.
+#
+# used for which run: Main.run
+#
+# this script is called by any other scripts: main_output.R
+#
+# this script calls other scripts: null
+#
+# functions called: null
+# 
+# input data: null
+#
+# output data: null
+#
+###############################################################################
 
 
 # note: here l refers to posterior sample
@@ -13,8 +35,8 @@ for (j in 1:C.adj) {
     adj.nodelta.jtl[j, t, ] <- c(mcmc.array[, , adj.parname])
     delta.l <- c(mcmc.array[, , paste0("delta.j[", j, "]")])
     adj.jtl[j, t, ] <- adj.nodelta.jtl[j, t, ] * delta.l
-  }#end of t loop
-}#end of j loop
+  } # end of t loop
+} # end of j loop
 
 selectADJ <- list(adj.jtl = adj.jtl,
                   adj.nodelta.jtl = adj.nodelta.jtl)
