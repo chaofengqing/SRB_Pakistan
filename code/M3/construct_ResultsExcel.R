27263047fc6dfd4f71877030ff6cfac4b8fa3ab6
qiqi <108138869+loofla@users.noreply.github.com>
2025-08-26 20:44:00 +0800

    Update construct_ResultsExcel.R
    add structured comments

--- code/M3/construct_ResultsExcel.R
+++ code/M3/construct_ResultsExcel.R
@@ -1,4 +1,23 @@
-
+##############################################################################
+# Levels and trends estimate of sex ratio at birth for seven provinces of Pakistan from 
+# 1980 to 2020 with scenario-based probabilistic projections 
+# of missing female birth to 2050: A Bayesian modeling approach
+# Code constructed by: Fengqing CHAO
+# Code last revised by: Qiqi Qiang on 26 Aug 2025
+#
+# construct_ResultsExcel.R
+#
+# This script saves provincial SRB estimations and projections in excel sheet.
+#
+# used for which run: main.run
+#
+# this script is called by any other scripts: main_output.R
+#
+# this script calls other scripts: null
+# functions called: null
+# input data: null
+# output data: data/output/SRB_by_PakistanProvince_estimates_1980_to_2020.csv
+###############################################################################
 
 ##################################################
 ## save provincial SRB estimates in excel sheet ##
@@ -20,7 +39,7 @@ for (c in 1:C) {
   add.data[, "Model.Estimate"] <- c(res.proj$R2.jqt[name.c[c], , paste(years.print.t)])
   
   data.out <- rbind(data.out, add.data)
-}#end of c loop
+} # end of c loop
 
 write.csv(data.out, row.names = FALSE, na = "",
           file = paste0(output.dir, "SRB_by_PakistanProvince_estimates_",
