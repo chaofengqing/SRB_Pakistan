b4e088128d0e3c49a838e33536db8d3b92ddbaf9
qiqi <108138869+loofla@users.noreply.github.com>
2025-08-27 14:26:08 +0800

    Update construct_countryCIs_SaveResults(cqt).R
    unfinished

--- code/M3/construct_countryCIs_SaveResults(cqt).R
+++ code/M3/construct_countryCIs_SaveResults(cqt).R
@@ -1,5 +1,35 @@
-
-
+#############################################################################
+# Levels and trends estimate of sex ratio at birth for seven provinces of Pakistan 
+# from 1980 to 2020 with scenario-based probabilistic projections 
+# of missing female birth to 2050: A Bayesian modeling approach
+#
+# Code constructed by: Fengqing CHAO
+# Code last revised by: Qiqi Qiang on 27 Aug 2025
+# construct_countryCIs_SaveResults(cqt).R
+# 
+# This script saves the results.
+#
+# used for which run: main.run
+#
+# this script is called by any other scripts: main_output.R
+#
+# this script calls other scripts: null
+#
+# functions called: function(2) means the function is called twice in this
+# script. Those functions called in the scripts listed above are not listed.
+# GetSexSpecificBirthfromAllandSRB(1)
+# 
+# input data: data/interim/birth.ct.rda
+#
+# output data: 
+# 1. data/output/countryTrajectory/trajectory_M3_Balochistan_full.rda
+# 2. data/output/countryTrajectory/trajectory_M3_Khyber Pakhtunkhwa_full.rda
+# 3. data/output/countryTrajectory/trajectory_M3_Punjab_full.rda
+# 4. data/output/countryTrajectory/trajectory_M3_Sindh_full.rda
+# 5. data/output/countryTrajectory/trajectory_M3_Gilgit Baltista_full.rda
+# 6. data/output/countryTrajectory/trajectory_M3_Islamabad (ICT)_full.rda
+# 7. data/output/countryTrajectory/trajectory_M3_Azad Jammu and Kashmir_full.rda
+# 8. data/output/countryTrajectory/trajectory_M3_Federally Administered Tribal Areas_full.rda
 ###############################################################################
 # SRB project
 #
