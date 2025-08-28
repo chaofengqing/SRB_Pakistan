9754435e247b0a58943c33dc34c541e1605d934f
qiqi <108138869+loofla@users.noreply.github.com>
2025-08-28 21:59:16 +0800

    Update jags_writeJAGSmodel.R
    add comments

--- code/M3/jags_writeJAGSmodel.R
+++ code/M3/jags_writeJAGSmodel.R
@@ -1,5 +1,26 @@
-
-
+###############################################################################
+# Levels and trends estimate of sex ratio at birth for seven provinces of Pakistan 
+# from 1980 to 2020 with scenario-based probabilistic projections 
+# of missing female birth to 2050: A Bayesian modeling approach
+#
+# Code constructed by: Fengqing CHAO
+# Code last revised by: Qiqi Qiang on 28 Aug 2025
+# 
+# jags_writeJAGSmodel.R
+# 
+# This script writes out JAGS model file.
+#
+# used for which run: main.run
+#
+# this script is called by any other scripts: jags_setupMCMC.R
+#
+# this script calls other scripts: null
+# functions called:                null
+# 
+# input data: most information are from source_BasicSetup and jags_setupMCMC.R.
+#
+# output data: data/output/M3/runname.txt
+#
 ###############################################################################
 
 # JAGS may not work well on sum with many elements. So we split into several
@@ -20,7 +41,7 @@ model {
     r.noadj.i[i] <- exp(logNmu + logP.jt[j.i[i], t.i[i]])
 
     tau.i[i] <- 1 / (pow(logSE.i[i], 2) + sigma.s^2)
-    }#end of i loop
+    } # end of i loop
     
     ##################################
     ## alpha.ct - adjustment factor ##
@@ -28,7 +49,7 @@ model {
     
     for (a in 2:A) {
     adjustment.a[a] <- alpha.jt[j.a[a], t.a[a]]
-    }#end of a loop
+    } # end of a loop
     
     for (j in 1:C.adj) {
     for (t in 1:Tend) {
@@ -46,7 +67,7 @@ model {
     beta3.jt[j, t] <- ((a.j[j] - (a.j[j] / D3.j[j]) *
     (t - T2.j[j])) * (t > T2.j[j] && t <= T3.j[j]))
     
-    }#end of k loop
+    } # end of k loop
     
     T1.j[j] <- (T0.j[j] + D1.j[j])
     T2.j[j] <- (T0.j[j] + D1.j[j] + D2.j[j])
@@ -60,7 +81,7 @@ model {
     
     # T0.j[j] ~ dt(index.TFRtarget.j[j], tau.TFRtarget, 3)
     T0.j[j] ~ dunif(pri.T0.c.lower, pri.T0.c.upper)
-    }#end of j loop
+    } # end of j loop
 
 
     ###############
@@ -70,8 +91,8 @@ model {
     logP.jt[j, 1] ~ dnorm(0, tau.eps.stat)
     for (t in 2:max.t.j[j]) {
     logP.jt[j, t] ~ dnorm(rho * logP.jt[j, t-1], tau.eps)
-    }#end of t loop
-    }#end of j loop
+    } # end of t loop
+    } # end of j loop
     
     
     ######################################
@@ -80,14 +101,13 @@ model {
     delta.j[j] ~ dbern(p.delta.j[j])
     p.delta.j[j] <- exp(q.delta.j[j]) / (exp(q.delta.j[j]) + 1)
     q.delta.j[j] ~ dnorm(delta.mu, tau.delta)
-    }#end of j loop
+    } # end of j loop
 
     delta.mu <- exp(delta.mu.probscale) / (exp(delta.mu.probscale) + 1)
     
 
     ############
     ## priors ##
-    # tau.TFRtarget <- pow(sigma.TFRtarget, -2)
     tau.eps.stat  <- tau.eps * (1 - pow(rho, 2))
     tau.eps       <- pow(sigma.eps, -2)
     tau.a.c <- pow(sigma.a.c, -2)
@@ -102,10 +122,9 @@ model {
     sigma.D3 <- pri.sigma.D3
 
     delta.mu.probscale ~ dunif(pri.delta.mu.probscale.lower, pri.delta.mu.probscale.upper)
-    # sigma.TFRtarget ~ dunif(pri.sigma.TFRtarget.lower, pri.sigma.TFRtarget.upper)
     sigma.delta ~ dunif(pri.sigma.delta.lower, pri.sigma.delta.upper)
     sigma.s ~ dunif(pri.sigma.s.lower, pri.sigma.s.upper)
-    }#end of model
+    } # end of model
     
     ",
     sep    = "",
