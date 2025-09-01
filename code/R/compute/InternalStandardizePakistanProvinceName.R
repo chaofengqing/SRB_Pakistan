de0bcad9b77454f5b0fbfba4daa2a919d73d8432
qiqi <108138869+loofla@users.noreply.github.com>
2025-09-01 13:59:58 +0800

    Update InternalStandardizePakistanProvinceName.R
    add comments

--- code/R/compute/InternalStandardizePakistanProvinceName.R
+++ code/R/compute/InternalStandardizePakistanProvinceName.R
@@ -1,4 +1,19 @@
-
+###############################################################################
+# Estimation and probabilistic projection of levels and trends 
+# in the sex ratio at birth in seven provinces of Nepal
+# from 1980 to 2050: a Bayesian modeling approach
+#
+# Code constructed by: Fengqing CHAO
+# Code last revised by: Qiqi Qiang on 8 Aug 2025
+# 
+# InternalStandardizeNepalDevRegName.R
+# 
+# This script contains all functions related to standardizing Pakistan's region name.
+# Functions are: function1(.., function2(3), ..); means function2 is called
+# three times inside function1.
+# InternalStandardizeNepalDevRegName(..)
+###############################################################################
+#------------------------------------------------------------------------------
 
 InternalStandardizePakistanProvinceName <- function(name.in) {
   
