d3772b5cbd1ae0f16a3f9de715c39bc6cb314cc2
qiqi <108138869+loofla@users.noreply.github.com>
2025-09-01 14:46:04 +0800

    Update getPostInfo.R
    Add comments

--- code/R/compute/getPostInfo.R
+++ code/R/compute/getPostInfo.R
@@ -1,6 +1,24 @@
-
-
-getPostInfo <- function(
+###############################################################################
+# Levels and trends estimate of sex ratio at birth for seven provinces of Pakistan 
+# from 1980 to 2020 with scenario-based probabilistic projections 
+# of missing female birth to 2050: A Bayesian modeling approach
+#
+# Code constructed by: Fengqing CHAO
+# Code last revised by: Qiqi Qiang on 29 Aug 2025
+# 
+# getPostInfo.R
+# 
+# This script contains function which extracts posterior summaries (percentiles and R-hat) 
+# for all parameters from an MCMC array.
+#
+# Functions are: function1(.., function2(3), ..); means function2 is called
+# three times inside function1.
+#
+# getPostInfo(..)
+#
+#
+###############################################################################
+#------------------------------------------------------------------------------getPostInfo <- function(
   mcmc.array,
   percentile.p = percentiles) {
   
@@ -11,7 +29,7 @@ getPostInfo <- function(
   for (i in 1:dim(mcmc.array)[3]) {
     post <- c(mcmc.array[, , i])
     POST.INFO[i, ] <- quantile(post, probs = percentile.p)
-  }#end of i loop
+  } # end of i loop
   
   post.full <- cbind(POST.INFO, Rhat)
   
@@ -24,11 +42,12 @@ getPostInfo <- function(
   for (i in 1:dim(post.full)[1]) {
     Percentile.Rhat[i] <- mean(post.full[i, "Rhat"] <= Rhat,
                                na.rm = TRUE) * 100
-  }#end of i loop
+  } # end of i loop
   
   post.full <- cbind(post.full, Percentile.Rhat)
   dimnames(post.full)[[2]] <- c(paste(percentile.p * 100, "percentile"),
                                 "Rhat (descending)",
                                 "Percentile.Rhat (100%)")  
   return(post.full)
-}#end of getPostInfo function
+} # end of getPostInfo function
+
