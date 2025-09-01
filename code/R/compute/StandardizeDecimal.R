4c74a77bcfd1243d7d506216c3d0b9a1d750debf
qiqi <108138869+loofla@users.noreply.github.com>
2025-09-01 14:08:34 +0800

    Update StandardizeDecimal.R
    add comments

--- code/R/compute/StandardizeDecimal.R
+++ code/R/compute/StandardizeDecimal.R
@@ -1,3 +1,23 @@
+###############################################################################
+# Levels and trends estimate of sex ratio at birth for seven provinces of Pakistan 
+# from 1980 to 2020 with scenario-based probabilistic projections 
+# of missing female birth to 2050: A Bayesian modeling approach
+#
+# Code constructed by: Fengqing CHAO
+# Code last revised by: Qiqi Qiang on 29 Aug 2025
+# 
+# StandardizeDecimal.R
+# 
+# This script contains function related to making number.in to round to n.round. 
+#
+# Functions are: function1(.., function2(3), ..); means function2 is called
+# three times inside function1.
+#
+# StandardizeDecimal(..)
+#
+#
+###############################################################################
+#------------------------------------------------------------------------------
 
 
 StandardizeDecimal <- function(
@@ -9,4 +29,5 @@ StandardizeDecimal <- function(
   number.out <- format(as.numeric(round(number.in, n.round)), nsmall = n.round)
   
   return(number.out)
-}#end of StandardizeDecimal()
+} # end of StandardizeDecimal()
+
